f8e2617609a4c3537638bf389089578f72cf8779
Deepa Porwal <er.deepakporwal@gmail.com>
2025-08-02 18:40:43 +0530

    post page removed

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+import Posts from '@/app/ui/posts'
+import { Suspense } from 'react'
+ 
+export default function Page() {
+  // Don't await the data fetching function
+  const posts = getPosts()
+ 
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Posts posts={posts} />
+    </Suspense>
+  )
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,16 +1,18 @@
 'use client';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 export default function ProductPage() {
     const params = useParams();
     const productId = params?.productId;
+    const searchParams = useSearchParams();
+    const category = searchParams.get('category');
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-200">
             <div className="bg-white rounded-xl shadow-lg p-10 flex flex-col items-center">
             <h1 className="text-4xl font-bold text-purple-700 mb-4">Product Details</h1>
             <p className="text-lg text-gray-700">
-                <span className="font-semibold">Product ID:</span> <span className="text-blue-600">{productId}</span>
+                <span className="font-semibold">Product ID:</span> <span className="text-blue-600">{productId} {category && `(${category})`}</span>
             </p>
             </div>
         </div>
